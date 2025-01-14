825ca339d73dce5b98f3e895650f09bf22a974b1
Ty Davis <tydavis.cougarfan@gmail.com>
2025-01-14 12:05:08 -0700

    Improved the test code for hw1

--- hw1/swapTemplateSingleFile.cpp
+++ hw1/swapTemplateSingleFile.cpp
@@ -47,12 +47,61 @@ void swapClass<Type>::swap(Type& first, Type& second) {
 	first = temp;
 }
 
+template <class Type>
+void printSwapClass(const swapClass<Type>& obj) {
+	std::cout << "Var 1: " << obj.var1 << '\n';
+	std::cout << "Var 2: " << obj.var2 << '\n';
+	std::cout << "Var 3: " << obj.var3 << '\n';
+}
+
+template <class Type>
+void testSwapClass(swapClass<Type>& obj) {
+	Type orig1 = obj.var1;
+	Type orig2 = obj.var2;
+	Type orig3 = obj.var3;
+	bool success = true;
+
+	std::cout << "Before swap:\n";
+	printSwapClass(obj);
+
+	std::cout <<  " -- Swap 1 & 2 -- \n";
+	obj.swap12();
+	success &= (orig1 == obj.var2 && orig2 == obj.var1 && orig3 == obj.var3);
+	// printSwapClass(obj);
+
+	std::cout <<  " -- Swap 1 & 3 -- \n";
+	obj.swap13();
+	success &= (orig1 == obj.var2 && orig2 == obj.var3 && orig3 == obj.var1);
+	// printSwapClass(obj);
+
+	std::cout <<  " -- Swap 2 & 3 -- \n";
+	obj.swap23();
+	success &= (orig1 == obj.var3 && orig2 == obj.var2 && orig3 == obj.var1);
+	// printSwapClass(obj);
+	
+	std::cout << "Result:\n";
+	printSwapClass(obj);
+	
+	if (success) {
+		std::cout << "Passed";
+	} else {
+		std::cout << "FAILED";
+	}
+}
+
 // Testing to make sure it runs
 int main(void) {
+	// Test with integers
 	swapClass<int> swapInts;
 	swapInts.var1 = 3;
 	swapInts.var2 = 5;
+	swapInts.var3 = -103202;
+	testSwapClass(swapInts);
 
-	swapInts.swap12();
-	printf("%d\n", swapInts.var1);
+	// Test with strings
+	swapClass<std::string> swapStrs;
+	swapStrs.var1 = "Windows";
+	swapStrs.var2 = "MacOS";
+	swapStrs.var3 = "Linux";
+	testSwapClass(swapStrs);
 }
